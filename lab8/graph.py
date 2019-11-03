13456cedd06e77570dc53507687a81e2aaa65737
Yusuf Bham <ybham6@gmail.com>
2019-11-02 22:12:54 -0400

    Lab8

---
+++ lab8/graph.py
@@ -0,0 +1,77 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+d = 6771000
+p = 10**5
+
+v = 7672
+p = 100
+
+t, x, y, d, vx, vy, mx, my = [], [], [], [], [], [], [], []
+with open("orbit.txt") as f:
+    for line in f:
+        j, ti, xi, yi, di, vxi, vyi, mxi, myi = map(float, line.split())
+        t.append(ti)
+        x.append(xi)
+        y.append(yi)
+        d.append(di)
+        vx.append(vxi)
+        vy.append(vyi)
+        mx.append(mxi)
+        my.append(myi)
+
+t = np.array(t)
+x = np.array(x)
+y = np.array(y)
+d = np.array(d)
+vx = np.array(vx)
+vy = np.array(vy)
+mx = np.array(mx)
+my = np.array(my)
+
+# ORBIT
+plt.subplot(121)
+plt.scatter(x, y, s=1, color="r")
+plt.axis('equal')
+plt.gca().set_xlim(0, max(max(x), max(mx)))
+plt.title("Orbit")
+
+# EARTH
+earth = plt.Circle((0, 0), 10*6.371e+6, color='blue', fill=False)
+plt.gca().add_artist(earth)
+
+# MOON
+plt.scatter(mx, my, s=1, color="g")
+
+# DISTANCE
+plt.subplot(222)
+plt.plot(t, d)
+plt.gca().set_xlim(0, max(t))
+
+plt.title("Distance")
+
+# SPEED
+plt.subplot(224)
+plt.plot(t, np.sqrt(vx**2 + vy**2))
+#  plt.axis('equal')
+plt.title("Speed")
+
+# plt.axis('off')
+plt.tight_layout()
+
+plt.savefig("graph.png")
+#plt.show()
+
+"""
+start = 120000
+for i in range(start, start + 20000, 500):
+    print(t[i], np.sqrt(vx[i]**2 + vy[i]**2))
+
+print(np.where(t == 228802.0))
+
+start = 132500
+for i in range(start - 500, start + 500, 50):
+    print(t[i], np.sqrt(vx[i]**2 + vy[i]**2))
+"""
+
+# print((228752 - 96302.0)/(60*60*24))
---
+++ lab8/lab8.c
@@ -0,0 +1,132 @@
+//
+// Yusuf Bham, 17 October 2019
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define G 6.674e-11 // (m^3)(kg^-1)(s^-2)
+#define M 5.972e+24 // kg
+#define R 6.371e+6  // m
+
+#define DT 0.25     // s
+
+#define M_MOON 7.349e+22 // kg
+#define R_MOON 1.7374e+6 // m
+#define V_MOON 1023.157  // m/s
+
+double* create_double_array(int n)
+{
+    return (double*) malloc(sizeof(double) * n);
+}
+
+int main(int argc, char** argv)
+{
+    if (argc == 1 || argc > 2)
+    {
+        fprintf(stderr, "Argument mismatch! Usage: [%s] THETA\n", argv[0]);
+        return 1;
+    }
+
+    //
+    // time intervals - duration is 90 minutes
+    //
+    int n = (int)( 0.5 + ( 80 * 60 * 60 ) / DT );
+
+    double*  t = create_double_array(n);
+
+    double*  x = create_double_array(n);
+    double*  y = create_double_array(n);
+    double* vx = create_double_array(n);
+    double* vy = create_double_array(n);
+    double* di = create_double_array(n);
+
+    double* x_moon = create_double_array(n);
+    double* y_moon = create_double_array(n);
+    double* vx_moon = create_double_array(n);
+    double* vy_moon = create_double_array(n);
+
+    FILE*  fout;
+
+    // International Space Station
+    //
+    // https://www.nasa.gov/sites/default/files/atoms/files/np-2015-05-022-jsc-iss-guide-2015-update-111015-508c.pdf
+    //
+    // Page 54 - altitude : 370 km to 460 km
+    // Page 54 - speed    : 28,000 km per hour
+
+    t[0]  =         95302.0;
+
+    double theta = atof(argv[1]) * (M_PI / 180);
+    double dist_mag = R + 202751774.4;
+    double v_mag = 1527.048;
+
+    x[0]  =  dist_mag * cos(theta);
+    y[0]  =  dist_mag * sin(theta);
+    di[0] =               dist_mag;
+    vx[0] =     v_mag * cos(theta);
+    vy[0] =     v_mag * sin(theta);
+
+    x_moon[0] =     3.844e8;
+    y_moon[0] =         0.0;
+    vx_moon[0] =        0.0;
+    vy_moon[0] =     V_MOON;
+
+    for(int j = 1; j < n; j++)
+    {
+        t[j] = t[j-1] + DT;
+
+        // EARTH => MOON
+        {
+            x_moon[j] = x_moon[j-1] + DT * vx_moon[j-1];
+            y_moon[j] = y_moon[j-1] + DT * vy_moon[j-1];
+
+            double distSq = x_moon[j]*x_moon[j] + y_moon[j]*y_moon[j];
+
+            double r = sqrt(distSq);
+
+            double a = -(G*M)/distSq;
+
+            vx_moon[j] = vx_moon[j - 1] + a * DT * x_moon[j] / r;
+            vy_moon[j] = vy_moon[j - 1] + a * DT * y_moon[j] / r;
+        }
+
+        // EARTH, MOON => SHIP
+        {
+            x[j] = x[j-1] + DT * vx[j-1];
+            y[j] = y[j-1] + DT * vy[j-1];
+    
+            double distSq = x[j]*x[j] + y[j]*y[j];
+
+            double r = sqrt(distSq);
+
+            di[j] = r;
+    
+            double a = -(G*M)/distSq;
+
+            double distSq_moon = pow(x[j] - x_moon[j], 2) + pow(y[j] - y_moon[j], 2);
+
+            double r_moon = sqrt(distSq_moon);
+
+            double a_moon = -(G*M_MOON)/distSq_moon;
+
+            vx[j] = vx[j-1] + (a * DT * x[j] / r) + (a_moon * DT * (x[j] - x_moon[j]) / r_moon);
+            vy[j] = vy[j-1] + (a * DT * y[j] / r) + (a_moon * DT * (y[j] - y_moon[j]) / r_moon);
+        }
+    }
+
+    fout = fopen( "orbit.txt" , "w" );
+
+    for(int j = 0; j < n; j++)
+    {
+        fprintf(fout, "%d %0.16f %0.16f %0.16f %0.16f %0.16f %0.16f %0.16f %0.16f\n", j, t[j], x[j], y[j], di[j], vx[j], vy[j], x_moon[j], y_moon[j]);
+    }
+
+    fclose( fout );
+    return 0;
+}
+
+//
+// end of file
+//
