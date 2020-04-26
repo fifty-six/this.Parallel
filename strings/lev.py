6f645d2842453bb8c6f9d7126c51ac4de813f2ef
Yusuf Bham <ybham6@gmail.com>
2020-04-26 01:28:06 -0400

    Lev DP

--- strings/lev.py
+++ strings/lev.py
@@ -3,66 +3,53 @@
 # Period 3
 #
 
-from pprint import pprint as e
-
 def lev_same_len(s1, s2):
     if len(s1) != len(s2):
         raise ValueError(f"Called on two strings of non-equal distance. {s1} {s2}")
 
     return sum(i != j for (i, j) in zip(s1, s2))
 
-def lev_one_off(s1, s2):
-    if abs(len(s1) - len(s2)) != 1:
-        raise ValueError("Called on two strings with length not differing by 1.")
-
-    small, big = sorted((s1, s2), key=len)
-
-    minimum = float("inf")
-
-    for ind in range(len(big)):
-        gapped = small[:ind] + "-" + small[ind:]
-
-        dist = lev_same_len(gapped, big)
-
-        minimum = min(dist, minimum)
-
-    return minimum
-
 
 def lev_recur(orig_s1, orig_s2):
-
-    small, big = sorted((orig_s1, orig_s2), key=len)
-
     def recur(s1, s2):
         if len(s1) == len(s2):
             return lev_same_len(s1, s2)
 
-        minimum = float("inf")
+        return min(recur(s1[:ind] + "-" + s1[ind:], s2) for ind in range(len(s2)))
 
-        for ind in range(len(s2)):
-            gapped = s1[:ind] + "-" + s1[ind:]
-            dist = recur(gapped, s2)
-            minimum = min(dist, minimum)
+    return recur(*sorted((orig_s1, orig_s2), key=len))
 
-        return minimum
 
-    return recur(small, big)
+def lev_dp(s1, s2):
+    # s1 x s2
+    mat = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+
+    for ind in range(len(s1) + 1):
+        mat[ind][0] = ind
+
+    for ind in range(len(s2) + 1):
+        mat[0][ind] = ind
+
+    for ind_i, i in enumerate(mat):
+        for ind_j, j in enumerate(i):
+            if ind_i == 0 or ind_j == 0:
+                continue
+
+            cost = 0 if s1[ind_i - 1] == s2[ind_j - 1] else 1
+
+            mat[ind_i][ind_j] = min(
+                mat[ind_i - 1][ind_j    ] + 1,
+                mat[ind_i    ][ind_j - 1] + 1,
+                mat[ind_i - 1][ind_j - 1] + cost,
+            )
+
+    return mat[len(s1) - 1][len(s2) - 1]
 
 
 def main():
-    # print("cat, bat:", lev_same_len("cat", "bat"))
-    # print("dog, cat:", lev_same_len("dog", "cat"))
+    w1, w2 = input("w1: "), input("w2: ")
+    print(lev_dp(w1, w2))
 
-    # print("kitchen, kitten:", lev_one_off("kitchen", "kitten"))
-    # print("meat, eat:", lev_one_off("meat", "eat"))
-    # print("eat, eats:", lev_one_off("eat", "eats"))
-
-    print("kitchen, kitten:", lev_recur("kitchen", "kitten"))
-    print("meat, eat:", lev_recur("meat", "eat"))
-    print("eat, eats:", lev_recur("eat", "eats"))
-    print("anagrams, alarms:", lev_recur("anagrams", "alarms"))
-
-    lev_dp("Saturday", "Sunday")
 
 if __name__ == "__main__":
     main()
